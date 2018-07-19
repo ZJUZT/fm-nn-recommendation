084b52b51d98aea60b9e9740ef2c8ff1b3bfefbe
lamontzhang <lamontzhang@tencent.com>
2018-07-19 05:34:50 -0700

    fix

--- model/DeepFM.py
+++ model/DeepFM.py
@@ -67,9 +67,9 @@ class DeepFM(torch.nn.Module):
     Attention: only support logsitcs regression
     """
 
-    def __init__(self, field_size, feature_sizes, embedding_size=4, is_shallow_dropout=True, dropout_shallow=[0.5, 0.5],
+    def __init__(self, field_size, feature_sizes, embedding_size=100, is_shallow_dropout=True, dropout_shallow=[0.5, 0.5],
                  h_depth=2, deep_layers=[32, 32], is_deep_dropout=True, dropout_deep=[0.5, 0.5, 0.5],
-                 deep_layers_activation='relu', n_epochs=64, batch_size=256, learning_rate=0.003,
+                 deep_layers_activation='relu', n_epochs=64, batch_size=256, learning_rate=0.01,
                  optimizer_type='adam', is_batch_norm=False, verbose=False, random_seed=950104, weight_decay=0.0,
                  use_fm=True, use_ffm=False, use_deep=True, loss_type='logloss', eval_metric=roc_auc_score,
                  use_cuda=True, n_class=1, greater_is_better=True
@@ -254,9 +254,10 @@ class DeepFM(torch.nn.Module):
                 # deep_emb = torch.cat([(torch.sum(emb(Xi[:, i, :]), 1).t() * Xv[:, i]).t() for i, emb in
                 #                       enumerate(self.fm_second_order_embeddings)], 1)
                 emb = self.fm_second_order_embeddings[0]
-                deep_emb = 0
+                deep_emb = []
                 for i in range(self.batch_size):
-                    deep_emb += emb(torch.LongTensor(Xi[i])).t() * torch.FloatTensor(Xv[i])
+                    deep_emb.append(torch.mm(emb(torch.LongTensor(Xi[i])).t(), torch.FloatTensor(Xv[i]).view(-1, 1)).t())
+                deep_emb = torch.cat(deep_emb, 0)
 
             if self.deep_layers_activation == 'sigmoid':
                 activation = F.sigmoid
